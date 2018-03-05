6398c0b692145506f8254e479f85066c4ebdb6d7
bbull <brian.bullington@gmail.com>
2018-03-04 19:51:32 -0600

    v4-pg67complete
    Section 3.3.4 Complete

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+ // router.get('/', function(req, res, next) {
+ //   res.render('index', { title: 'Express' });
+ // });
+
+
+
+
+
+ 
+var homepageController = function(req, res){
+  res.render('index', {title:'Express'});
+};
+
+/*Get home page . */
+
+router.get('/', homepageController);
+
 
 module.exports = router;
