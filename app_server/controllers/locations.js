437d50c841ab7c60180f040a13e80c150d8d992f
bbull <brian.bullington@gmail.com>
2018-03-08 00:37:06 -0600

    pg101IndividualPage
    Description Page built except for the google map API

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -7,7 +7,7 @@ module.exports.homelist =function(req,res){
 
 /*GET 'location info' page */
 module.exports.locationInfo =function(req,res){
-    res.render('index.jade', {title:'Location Info'});  //.jade added for clarity. This sends for index.jade, giving it a variable for title
+    res.render('location-info.jade', {title:'Location Info'});  //.jade added for clarity. This sends for index.jade, giving it a variable for title
 };
 
 /*GET 'addReview' page */
