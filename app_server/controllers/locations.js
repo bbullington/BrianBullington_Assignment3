930f9dd67a03184a552a78b667857454923258f6
bbull <brian.bullington@gmail.com>
2018-03-08 02:13:17 -0600

    pg118AllButTheOtherViews
    Everything Except for pulling data from the unlisted views.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -2,8 +2,35 @@
 
 /*GET 'home' page */
 module.exports.homelist =function(req,res){
-  res.render('locations-list.jade', {title:'Home'});  //.jade added for clarity. This sends for index.jade, giving it a variable for title
+  res.render('locations-list.jade', {
+      title: 'Loc8r - find a place to work with wifi',
+      sidebar: "So you are looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+      pageHeader: {
+          title: 'Loc8r',
+          strapline: 'Find places to work with wifi near you!!!!!!'
+      },
+      locations: [{
+          name: 'Starcups',
+          address: '125 High Street, Reading, RG6, 1PS',
+          rating: 3,
+          facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+          distance: '100m'
+      },{
+          name: 'Devon Coffee',
+          address: '125 Low Street, Science, RG6, 1PS',
+          rating: 4,
+          facilities: ['Hot drinks', 'Premium wifi'],
+          distance: '200m'
+      },{
+          name: 'Carla Cofee',
+          address: '36 Low Street, Tutor, RG6, 1PS',
+          rating: 2,
+          facilities: ['Food','Premium wifi'],
+          distance: '200m'
+      }]
+  });
 };
+
 
 /*GET 'location info' page */
 module.exports.locationInfo =function(req,res){
@@ -14,3 +41,25 @@ module.exports.locationInfo =function(req,res){
 module.exports.addReview =function(req,res){
     res.render('location-review-form.jade', {title:'Add Review'});  //.jade added for clarity. This sends for index.jade, giving it a variable for title
 };
+
+
+
+locations: [{
+    name: 'Starcups',
+    address: '125 High Street, Reading, RG6, 1PS',
+    rating: 3,
+    facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+    distance: '100m'
+},{
+    name: 'Devon Coffee',
+    address: '125 Low Street, Science, RG6, 1PS',
+    rating: 4,
+    facilities: ['Hot drinks', 'Premium wifi'],
+    distance: '200m'
+},{
+    name: 'Carla Cofee',
+    address: '36 Low Street, Tutor, RG6, 1PS',
+    rating: 2,
+    facilities: ['Food','Premium wifi'],
+    distance: '200m'
+}]
