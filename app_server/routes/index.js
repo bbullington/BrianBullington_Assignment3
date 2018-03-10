8a8361706be1b68ba21026ef298d653f3e29eafa
bbull <brian.bullington@gmail.com>
2018-03-10 16:44:13 -0600

    Adding preview test
    preview text ready for all three files.

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,49 +1,27 @@
 var express = require('express');
 var router = express.Router();
-var ctrlLocations = require('../controllers/locations.js');
-var ctrlOthers= require('../controllers/others.js');
 
-//var ctrlMain=require('../controllers/main.js'); //.js added for clarity. pg 69 commented
+
+var ctrlBbnotes = require('../controllers/bbnotes.js'); //formerly:  var ctrlLocations = require('../controllers/locations.js');
+var ctrlBbNotesLogin = require('../controllers/bbnotesLogin.js')
+//var ctrlOthers= require('../controllers/others.js');
+
+
 
 
 /*Locations pages*/      //routes to controllers/locations.js when URL comes in
-router.get('/', ctrlLocations.homelist);
-router.get('/location', ctrlLocations.locationInfo);
-router.get('/location/review/new', ctrlLocations.addReview);
+router.get('/notes', ctrlBbnotes.notesList);
+router.get('/newnote', ctrlBbnotes.noteEdit);
+
+
+/*Login Page */     //routes to controllers/others.js when URL comes in
+
+router.get('/', ctrlBbNotesLogin.login);
+//router.get('/location/review/new', ctrlLocations.addReview);
 
 /*Other pages */     //routes to controllers/others.js when URL comes in
-router.get('/about', ctrlOthers.about);
+//router.get('/about', ctrlOthers.about);
 
 module.exports = router;
 
-
-
-
-/*old code*/
-
-
-
-/* GET home page. */
-//router.get('/', ctrlMain.index); commented out pg 84
-
-
-
-
-
-
- // router.get('/', function(req, res, next) {
- //   res.render('index', { title: 'Express' });
- // });
-
-
-//router.get('/', homepageController);
-
-
-
-/* GET home page. */
-
-// var homepageController = function(req, res){
-//   res.render('index.jade', {title:'Express'});  //.jade added for clarity. This sends for index.jade, giving it a variable for title
-// };
-//
 
