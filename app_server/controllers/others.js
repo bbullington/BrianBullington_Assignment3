0b0b2ba4d681d9198a7ea2bcd4d55b368b1a84c3
bbull <brian.bullington@gmail.com>
2018-03-10 21:16:12 -0600

    cleaned up- nav bar and logger left

--- app_server/controllers/others.js
+++
@@ -1,9 +0,0 @@
-
-
-/*GET 'about' page */
-module.exports.about=function(req,res){
-  res.render('generic-text.jade', {title:'About'});  //.jade added for clarity. This sends for index.jade, giving it a variable for title
-};
-
-
-
